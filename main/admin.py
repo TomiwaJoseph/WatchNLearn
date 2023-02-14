b7de79bb1a491037418e7bfcac839f90ffb8207d
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-02-14 20:46:38 +0100

    first commit

--- main/admin.py
+++ main/admin.py
@@ -5,8 +5,8 @@ from .models import Newsletter, LandingPagePictures, InstructorPagePictures
 admin.site.register(Newsletter)
 admin.site.register(InstructorPagePictures)
 
+
 @admin.register(LandingPagePictures)
 class LandingPictures(admin.ModelAdmin):
     list_display = ['caption', 'image_tag']
     prepopulated_fields = {'slug': ('caption',)}
-    
--- main/views.py
+++ main/views.py
@@ -22,13 +22,15 @@ def newsletter(request):
 
 # Ajax Requests Ends
 
+
 def index(request):
-    random_pick = randint(0, LandingPagePictures.objects.count() -1)
+    random_pick = randint(0, LandingPagePictures.objects.count() - 1)
     random_picture = LandingPagePictures.objects.all()[random_pick]
     context = {
         'random_picture': random_picture,
     }
     return render(request, 'main/index.html', context)
+
 
 def contact(request):
     if request.method == 'POST':
@@ -36,66 +38,69 @@ def contact(request):
         subject = request.POST.get('subject')
         email = request.POST.get('email')
         message = request.POST.get('message')
-        
+
         intro_and_message = f"Hi, {name} here.\n" + message
-            
+
         send_mail(subject, intro_and_message, email,
-            [settings.EMAIL_HOST_USER], fail_silently=False)
+                  [settings.EMAIL_HOST_USER], fail_silently=False)
 
         messages.success(request, 'Message sent successfully')
         return redirect('contact')
     return render(request, 'main/contact.html')
+
 
 def search_course(request):
     output = []
     search_input = request.POST.get('search')
     all_words_in_search_input = search_input.split()
     search_results = []
-    
+
     def removeNestings(array):
         for item in array:
             if type(item) == list:
                 removeNestings(item)
-            else: output.append(item)
-    
+            else:
+                output.append(item)
+
     for word in all_words_in_search_input:
         query = list(Course.objects.filter(
             Q(title__contains=word) |
             Q(keywords__name__icontains=word)
         ).distinct())
         search_results.append(query)
-        
+
     removeNestings(search_results)
-    
+
     context = {
         'search_input': search_input,
         'search_results': output,
     }
     return render(request, 'main/search.html', context)
 
+
 def tag_search(request, tag):
     output = []
     all_words_in_search_input = tag.split("-")
     search_results = []
-    
+
     def removeNestings(array):
         for item in array:
             if type(item) == list:
                 removeNestings(item)
-            else: output.append(item)
-    
+            else:
+                output.append(item)
+
     for word in all_words_in_search_input:
         query = list(Course.objects.filter(
             Q(title__contains=word) |
             Q(keywords__name__icontains=word)
         ).distinct())
         search_results.append(query)
-        
+
     removeNestings(search_results)
-    
+
     context = {
         'search_input': all_words_in_search_input,
         'search_results': list(set(output)),
     }
     return render(request, 'main/tag_search.html', context)
-
