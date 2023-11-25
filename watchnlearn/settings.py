84a98614e95d1e3e8849793ff865aff5276d3b85
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-11-25 07:57:28 -0800

    removed postgresql as database

--- watchnlearn/settings.py
+++ watchnlearn/settings.py
@@ -75,17 +75,17 @@ WSGI_APPLICATION = 'watchnlearn.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # Sqlite Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Postgresql Database
 DATABASES = {
-    "default": dj_database_url.parse(config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# # Postgresql Database
+# DATABASES = {
+#     "default": dj_database_url.parse(config('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
